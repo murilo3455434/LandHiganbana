7682d9e67f6ca7c453e88ac8d3834341d8f71327
murilo3455434 <masmendes22@gmail.com>
2025-08-08 12:53:09 -0300

    Teste

--- Landpage/script.js
+++ Landpage/script.js
@@ -6,54 +6,50 @@ const nome = document.getElementsByClassName("nome-personagem")[0];
 const Historia = document.getElementsByClassName("texto-personagem")[0];
 
 function IndexPer() {
+  const params = new URLSearchParams(window.location.search);
+  const personagemID = params.get('');
 
-  if (href === "http://127.0.0.1:5500/Landpage/personagem.html?1") {
-    
-    
-    if (nome) {
-      nome.innerText = "Thiago José Fritz";
-      Historia.innerText = "Thiago é o que podemo chamar de CLT médio. nasceu em uma capital. desde criança sempre gostou de entender e desvendar todo tipo de mistério. Essa curiosidade provavelmente foi fruto de conviver com seu pai, que por anos durante a infância de Thiago, foi cientista forense. Entender provas e dados se tornou a paixáo do jovem Fritz. Sua carisma e otimismo vieram de sua mae, uma ótima psiquiatra que sempre o fez enxergar que o copo que está vazio ao menos não vai transbordar. Seus esforços e dedicação formaram seu currículo com dois anos de estágio em um jornal local e formaçdo em jornalismo em uma faculdade bem reconhecida.";
-      document.getElementById("Imageper").src = "./images/sketch 1-Thiago.png";
-    }
-  }
+  if (!personagemID) return;
 
-  if (href === "http://127.0.0.1:5500/Landpage/personagem.html?2") {
-    const nome = document.getElementsByClassName("nome-personagem")[0];
-    if (nome) {
-      nome.innerText = "Jhonatan Pinto Da Silva";
-      Historia.innerText = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
-      document.getElementById("Imageper").src = "./images/sketch 1-Thiago.png";
-    }
-  }
+  const nomeEl = document.getElementsByClassName("nome-personagem")[0];
+  const historiaEl = document.getElementsByClassName("texto-personagem")[0];
+  const imageEl = document.getElementById("Imageper");
 
-  if (href === "http://127.0.0.1:5500/Landpage/personagem.html?3") {
-    const nome = document.getElementsByClassName("nome-personagem")[0];
-    if (nome) {
-      nome.innerText = "Personagem 2";
-      Historia.innerText = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
-      document.getElementById("Imageper").src = "./images/sketch 1-Thiago.png";
-    }
-  }
+  if (!nomeEl || !historiaEl || !imageEl) return;
 
-  if (href === "http://127.0.0.1:5500/Landpage/personagem.html?4") {
-    const nome = document.getElementsByClassName("nome-personagem")[0];
-    if (nome) {
-      nome.innerText = "Personagem 3";
-      Historia.innerText = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
-      document.getElementById("Imageper").src = "./images/sketch 1-Thiago.png";
-    }
-  }
+  switch (personagemID) {
+    case "1":
+      nomeEl.innerText = "Thiago José Fritz";
+      historiaEl.innerText = "Thiago é o que podemo chamar de CLT médio. nasceu em uma capital. desde criança sempre gostou de entender e desvendar todo tipo de mistério. Essa curiosidade provavelmente foi fruto de conviver com seu pai, que por anos durante a infância de Thiago, foi cientista forense. Entender provas e dados se tornou a paixáo do jovem Fritz. Sua carisma e otimismo vieram de sua mae, uma ótima psiquiatra que sempre o fez enxergar que o copo que está vazio ao menos não vai transbordar. Seus esforços e dedicação formaram seu currículo com dois anos de estágio em um jornal local e formaçdo em jornalismo em uma faculdade bem reconhecida.";
+      imageEl.src = "./images/sketch 1-Thiago.png";
+      break;
 
-  if (href === "http://127.0.0.1:5500/Landpage/personagem.html?5") {
-    const nome = document.getElementsByClassName("nome-personagem")[0];
-    if (nome) {
-      nome.innerText = "Personagem 4";
-      Historia.innerText = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
-      document.getElementById("Imageper").src = "./images/sketch 1-Thiago.png";
-    }
+    case "2":
+      nomeEl.innerText = "Jhonatan Pinto Da Silva";
+      historiaEl.innerText = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+      imageEl.src = "./images/sketch 1-Thiago.png";
+      break;
+
+    case "3":
+      nomeEl.innerText = "Personagem 2";
+      historiaEl.innerText = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+      imageEl.src = "./images/sketch 1-Thiago.png";
+      break;
+
+    case "4":
+      nomeEl.innerText = "Personagem 3";
+      historiaEl.innerText = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+      imageEl.src = "./images/sketch 1-Thiago.png";
+      break;
+
+    case "5":
+      nomeEl.innerText = "Personagem 4";
+      historiaEl.innerText = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+      imageEl.src = "./images/sketch 1-Thiago.png";
+      break;
   }
- 
 }
+
 
 function Personagem(n){
   window.location.href = "http://127.0.0.1:5500/Landpage/personagem.html?"+n;
